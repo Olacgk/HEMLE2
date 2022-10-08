d2b5238dd2ed703b40ec47326d3063586a152b2e
Theotime Chognika <94149002+Olacgk@users.noreply.github.com>
2022-10-08 14:54:44 +0100

    Header without responsive

--- hemle_groupwork/src/components/Header.js
+++ hemle_groupwork/src/components/Header.js
@@ -1,37 +1,28 @@
-import React, {useState} from "react";
+import React from "react";
 import {FaSearch} from "react-icons/fa";
-import {MdClose, MdLanguage, MdMenu} from "react-icons/md";
+import {MdLanguage} from "react-icons/md";
 import {AiOutlineDown} from "react-icons/ai";
 import logo1 from "../assets/Logo_Hemlè-V2-39.svg";
 import "./Header.css"
 
 const Header = () => {
-    const [open, setOpen] = useState(false);
+    //const [open, setOpen] = useState(false);
     return(
         <header>
-            <div className={'logo'}>
-                <img alt={'Logo Hemlè'} src={logo1}/>
-                {/*<div onClick={() =>(setOpen(!open))} className={"cursor-pointer md:hidden text-3xl"}>*/}
-                {/*    {open ? <MdClose/> : <MdMenu/>}*/}
-                {/*</div>*/}
-            </div>
-            <div className={'items'}>
-                <nav>
-                    <ul>
-                        <li>ACCUEIL</li>
-                        <li className={'prestation'}>
-                            <p>PRESTATIONS</p>
-                            <AiOutlineDown/>
-                        </li>
-                        <li>NOS CLIENTS </li>
-                        <li>A PROPOS</li>
-                    </ul>
-                </nav>
-                <div className={'button'}>
-                    <FaSearch className={'buttonchild'} size='1rem'/>
-                    <MdLanguage className={'buttonchild'} size='1.5rem'/>
-                    <button className={'buttonchild'}>SE CONNECTER</button>
-                </div>
+            <img className={'logo'} alt={'Logo Hemlè'} src={logo1}/>
+            <ul className={'items'}>
+                <li>ACCUEIL</li>
+                <li className={'prestation'}>
+                    <p>PRESTATIONS</p>
+                    <AiOutlineDown/>
+                </li>
+                <li>NOS CLIENTS </li>
+                <a href={'#'}> <li className={'prestation'}>A PROPOS</li> </a>
+            </ul>
+            <div className={'button'}>
+                <FaSearch className={'buttonchild'} size='1rem'/>
+                <MdLanguage className={'buttonchild'} size='1.5rem'/>
+                <button className={'buttonchild1'}>SE CONNECTER</button>
             </div>
         </header>
     )
