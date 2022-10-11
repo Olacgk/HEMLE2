4221ad6569af01cd957a108c21d921f7b5849e92
Theotime Chognika <94149002+Olacgk@users.noreply.github.com>
2022-10-11 15:17:58 +0100

    Header refactor

--- hemle_groupwork/src/components/Header/index.js
+++ hemle_groupwork/src/components/Header/index.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { Link } from "react-router-dom"
-import {FaSearch} from "react-icons/fa";
-import {MdLanguage} from "react-icons/md";
+import {FaBars, FaSearch} from "react-icons/fa";
+import {MdClose, MdLanguage} from "react-icons/md";
 import {AiOutlineDown} from "react-icons/ai";
 import logo1 from "../../assets/Logo_Hemlè-V2-39.svg";
 import "./style.css"
 
 const Header = () => {
-    //const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
     return(
         <header>
             <Link to={'/'}>
@@ -17,10 +17,10 @@ const Header = () => {
             <ul className={'items'}>
                 <li>ACCUEIL</li>
                 <li className={'prestation'}>
-                    <p>PRESTATIONS</p>
+                    <p>PAYS/CLUBS</p>
                     <AiOutlineDown/>
                 </li>
-                <li>NOS CLIENTS </li>
+                <li>ETOILE D'OR </li>
                 <li>
                     <Link className={'prestation'} to="/about">A PROPOS</Link>
                 </li>
@@ -30,6 +30,7 @@ const Header = () => {
                 <MdLanguage className={'buttonchild'} size='2rem'/>
                 <button className={'buttonchild1'}>SE CONNECTER</button>
             </div>
+            {open ? <FaBars className={'menu'} onClick={()=>setOpen(!open)}/> : <MdClose className={'menu'} onClick={()=>setOpen(!open)}/>}
         </header>
     )
 }
