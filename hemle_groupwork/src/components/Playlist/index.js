41e5e7ef8184ca2af3a6743aa3c42f6c8128f2bb
Theotime Chognika <94149002+Olacgk@users.noreply.github.com>
2022-10-12 10:58:14 +0100

    Playlist component

--- hemle_groupwork/src/components/Playlist/index.js
+++ hemle_groupwork/src/components/Playlist/index.js
@@ -9,23 +9,23 @@ const Playlist = () => {
     const handleClick = () => {
         setOpen(!open)
     }
+    ;
 
   return(
       <div>
           <div className={'container'}>
               <p className={'playlist'}>Playlist</p>
               {
-                  playlistContent.map( playlist => {
+                  playlistContent.filter(playlist => playlist.id < 6).map( playlist => {
                       return (
                           <PlaylistCard key={playlist.id} handleClick={handleClick} date={playlist.date} id={playlist.id} photo={playlist.photo} title={playlist.title}/>
                       )
                   } )
               }
+              <div className="boutoncenter">
+                <button className="playlistShowMore">VOIR TOUT</button>
+              </div>
           </div>
-          {/* {
-              open? <Playerbottom handleClick={handleClick}/> : <></>
-          } */}
-
       </div>
   )
 }
