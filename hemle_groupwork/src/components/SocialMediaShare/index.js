d35800d77cd24263d7164c919afe1640fb815fcc
Olacgk <theotimechognika@gmail.com>
2022-11-08 18:39:56 +0100

    social media share

--- hemle_groupwork/src/components/SocialMediaShare/index.js
+++ hemle_groupwork/src/components/SocialMediaShare/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import {FaFacebook, FaTwitter, FaInstagram, FaWhatsapp, FaTiktok} from 'react-icons/fa'
+import {MdOutlineShare} from 'react-icons/md'
 
 export default function SocialMediaShare() {
+
+    const [open, setOpen] = useState(false)
+
+    const shareClick = ()=>{
+        setOpen(!open)
+        open ? document.getElementsByClassName('showSocialMedia')[0].style.display = "flex" : document.getElementsByClassName('showSocialMedia')[0].style.display = "none"
+    }
+
   return (
-    <div>SocialMediaShare</div>
+    <div className='socialMediaShare'>
+        <div className='showSocialMedia'>
+            <p>Partager sur:</p>
+            <div className='socialmedia'>
+                <FaFacebook/>
+                <FaTwitter/>
+                <FaInstagram/>
+                <FaWhatsapp/>
+                <FaTiktok/>
+            </div>
+        </div>
+        <MdOutlineShare className='shareButton' onClick={shareClick}/>
+    </div>
   )
 }
